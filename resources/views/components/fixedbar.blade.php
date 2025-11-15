3419f4cb59e45d69e380428cbf8825e03df7f75c
Anperezna <tete.perez.moreno@gmail.com>
2025-11-15 14:48:27 +0100

    Iconos con transiciones

--- resources/views/components/fixedbar.blade.php
+++ resources/views/components/fixedbar.blade.php
@@ -22,7 +22,8 @@
     class="flex flex-col items-center fixed left-0 bottom-0 justify-between {{ $width }} {{ $topSpace }} {{ $bg }} {{ $margin }} {{ $rounded }} {{ $padding }}">
     <div class="flex flex-col items-center pt-8" style="gap: 40px;">
         @foreach ($iconosArriba as $item)
-            <a href="{{ route($item['ruta']) }}" class="hover:opacity-80 transition-opacity">
+            <a href="{{ route($item['ruta']) }}"
+                class="hover:opacity-80 hover:scale-110 transition-all duration-200 ease-in-out">
                 <img src="{{ asset('images/fixedbar_icons/' . $item['icono']) }}" alt="{{ $item['icono'] }}"
                     class="{{ $iconSize }}">
             </a>
@@ -30,7 +31,8 @@
     </div>
 
     <div class="pb-8">
-        <a href="{{ route($iconoAbajo['ruta']) }}" class="hover:opacity-80 transition-opacity">
+        <a href="{{ route($iconoAbajo['ruta']) }}"
+            class="hover:opacity-80 hover:scale-105 transition-all duration-200 ease-in-out">
             <img src="{{ asset('images/fixedbar_icons/' . $iconoAbajo['icono']) }}" alt="{{ $iconoAbajo['icono'] }}"
                 class="{{ $iconSize }}">
         </a>
