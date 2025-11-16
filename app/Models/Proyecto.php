60eefe5b414277cfa00ea38f81e7ba533f700aa2
Anperezna <tete.perez.moreno@gmail.com>
2025-11-16 12:35:00 +0100

    modelos creados

---
+++ app/Models/Proyecto.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Proyecto extends Model
+{
+    //
+    protected $table = 'Proyecto';
+
+    // private $primaryKey = 'id';
+    public $timestamps = false;
+
+    public $incrementing = false;
+}
