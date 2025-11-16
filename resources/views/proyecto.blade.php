d4ea016ddb7b96c0f0fad17d2a3b018fc536ff5b
ddelacortep <ddelacortep@gmail.com>
2025-11-16 21:21:06 +0100

    primera publicacion de la rama

--- resources/views/proyecto.blade.php
+++ resources/views/proyecto.blade.php
@@ -33,7 +33,7 @@
                         </div>
                     </div>
                     <div>
-                        @
+                        
                     </div>
                 </x-gestionproyecto>
             </div>
